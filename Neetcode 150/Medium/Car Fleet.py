802697eb040bb2cfc6a99766fd693f0a1f00e857
AVILASH TAMANG <avilashtamang@AVILASHs-MacBook-Pro.local>
2026-07-04 18:14:56 +0530

    Car FLeet

---
+++ Neetcode 150/Medium/Car Fleet.py
@@ -0,0 +1,26 @@
+# There are n cars traveling to the same destination on a one-lane highway.
+# You are given two arrays of integers position and speed, both of length n.
+# position[i] is the position of the ith car (in miles)
+# speed[i] is the speed of the ith car (in miles per hour)
+# The destination is at position target miles.
+# A car can not pass another car ahead of it. It can only catch up to another car and then drive at the same speed as the car ahead of it.
+# A car fleet is a non-empty set of cars driving at the same position and same speed. A single car is also considered a car fleet.
+# If a car catches up to a car fleet the moment the fleet reaches the destination, then the car is considered to be part of the fleet.
+# Return the number of different car fleets that will arrive at the destination.
+
+from typing import List
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        fleet, max_time = 0, 0
+        for i in range(len(position)):
+            time = (target - position[i])/speed[i]
+            if time > max_time:
+                fleet += 1
+                max_time = time
+        return fleet
+
+if __name__ == '__main__':
+    target = 10
+    position = [4,1,0,7]
+    speed = [2,2,1,1]
+    print(Solution().carFleet(target,position,speed))
